9484dcb3b2a5b9e134e55bc4ba40a644607c1762
Georgio Nassar <georgionassar16@gmail.com>
2025-09-07 20:55:47 +0300

    feat: add pagination

--- tremo-app/app/index.tsx
+++ tremo-app/app/index.tsx
@@ -1,11 +1,19 @@
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, ViewToken } from "react-native";
 import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
 import data, { OnboardingData } from '../data/onboarding';
 import RenderItem from "@/components/RenderItem";
+import Pagination from "@/components/Pagination";
 
 export default function Index() {
   const flatlistRef = useAnimatedRef<FlatList<OnboardingData>>();
   const x = useSharedValue(0);
+  const flatlistIndex = useSharedValue(0);
+
+  const onViewableItemsChanged = ({viewableItems}:{viewableItems: ViewToken[]}) => {
+    if(viewableItems[0].index !== null) {
+      flatlistIndex.value = viewableItems[0].index;
+    }
+  }
 
   const onScroll = useAnimatedScrollHandler({
     onScroll: event => {
@@ -28,7 +36,15 @@ export default function Index() {
       bounces={false}
       pagingEnabled={true}
       showsHorizontalScrollIndicator={false}
+      onViewableItemsChanged={onViewableItemsChanged}
+      viewabilityConfig={{
+        minimumViewTime: 300,
+        viewAreaCoveragePercentThreshold: 10
+      }}
       />
+      <View style={styles.bottomContainer}>
+        <Pagination data={data} x={x}/>
+      </View>
     </View>
   );
 }
@@ -36,5 +52,13 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  bottomContainer : {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    marginHorizontal: 30,
+    paddingVertical: 30
   }
 });
---
+++ tremo-app/components/Dot.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
+import React from 'react'
+import Animated, { interpolate, interpolateColor, SharedValue, useAnimatedStyle } from 'react-native-reanimated';
+
+type Props = {
+    index: number;
+    x: SharedValue<number>
+}
+
+const Dot = ({index, x}: Props) => {
+    const {width: SCREEN_WIDTH} = useWindowDimensions();
+
+    const animatedDotStyle = useAnimatedStyle(() => {
+        const widthAnimation = interpolate(
+            x.value,
+            [
+                (index - 1) * SCREEN_WIDTH,
+                index * SCREEN_WIDTH,
+                (index + 1) * SCREEN_WIDTH
+            ],
+            [10, 20, 10],
+            "clamp"
+        );
+        const opacityAnimation = interpolate(
+            x.value,
+            [
+                (index - 1) * SCREEN_WIDTH,
+                index * SCREEN_WIDTH,
+                (index + 1) * SCREEN_WIDTH
+            ],
+            [0.5, 1, 0.5],
+            "clamp"
+        );
+        return {
+            width: widthAnimation,
+            opacity: opacityAnimation
+        };
+    });
+
+    const animatedColor = useAnimatedStyle(() => {
+        const backgroundColor = interpolateColor(
+            x.value,
+            [0, SCREEN_WIDTH, 2 * SCREEN_WIDTH],
+            ['#005b4f', '#1e2169', '#f15937']
+        );
+        return {
+            backgroundColor: backgroundColor
+        }
+    })
+
+    return (
+        <Animated.View style={[styles.dot, animatedDotStyle, animatedColor]}/>
+    )
+}
+
+export default Dot
+
+const styles = StyleSheet.create({
+    dot: {
+        height: 10,
+        borderRadius: 5,
+        marginHorizontal: 10
+    }
+})
