752e925dc18c313ba3ffd83c3246690da3deb1f2
Georgio Nassar <georgionassar16@gmail.com>
2025-09-07 20:14:02 +0300

    feat: update onboarding screen design

--- tremo-app/app/index.tsx
+++ tremo-app/app/index.tsx
@@ -1,13 +1,26 @@
 import { FlatList, StyleSheet, View } from "react-native";
-import Animated from 'react-native-reanimated';
-import data from '../data/onboarding';
+import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import data, { OnboardingData } from '../data/onboarding';
 import RenderItem from "@/components/RenderItem";
 
 export default function Index() {
+  const flatlistRef = useAnimatedRef<FlatList<OnboardingData>>();
+  const x = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      x.value = event.contentOffset.x;
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Animated.FlatList data={data} renderItem={({item, index}) => {
-        return <RenderItem item={item} index={index} />
+      <Animated.FlatList 
+      ref={flatlistRef}
+      onScroll={onScroll}
+      data={data}
+      renderItem={({item, index}) => {
+        return <RenderItem item={item} index={index} x={x} />
       }}
       keyExtractor={item => item.id}
       scrollEventThrottle={16}
--- tremo-app/components/RenderItem.tsx
+++ tremo-app/components/RenderItem.tsx
@@ -2,24 +2,70 @@ import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import React from 'react'
 import { OnboardingData } from '@/data/onboarding'
 import LottieView from 'lottie-react-native';
+import Animated, { interpolate, SharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
 type Props = {
     item: OnboardingData;
     index: number;
+    x: SharedValue<number>
 }
 
-const RenderItem = ({item, index}: Props) => {
+const RenderItem = ({item, index, x}: Props) => {
     const {width: SCREEN_WIDTH} = useWindowDimensions();
+
+    const lottieAnimationStyle = useAnimatedStyle(() => {
+        const translateYAnimation = interpolate(
+            x.value,
+            [
+                (index - 1) * SCREEN_WIDTH,         //reference point to left of current element's position
+                index * SCREEN_WIDTH,               //reference point to center of current element's position
+                (index + 1) * SCREEN_WIDTH,         //reference point to right of current element's position
+            ],
+            [200, 0, -200],
+            "clamp"
+        );
+        return {
+            transform: [{translateY: translateYAnimation}]
+        }
+    })
+
+    const circleAnimation = useAnimatedStyle(() => {
+        const scale = interpolate(
+            x.value,
+            [
+                (index - 1) * SCREEN_WIDTH,         //reference point to left of current element's position
+                index * SCREEN_WIDTH,               //reference point to center of current element's position
+                (index + 1) * SCREEN_WIDTH,         //reference point to right of current element's position
+            ],
+            [1, 4, 4],
+            "clamp"
+        );
+        return {
+            transform: [{scale: scale}]
+        }
+    })
+
     return (
         <View style={[styles.itemContainer, {width: SCREEN_WIDTH}]}>
-            <View>
+            <View style={styles.circleContainer}>
+                <Animated.View
+                    style={[{
+                        width: SCREEN_WIDTH, 
+                        height: SCREEN_WIDTH, 
+                        backgroundColor: item.backgroundColor,
+                        borderRadius: SCREEN_WIDTH / 2
+                    },
+                    circleAnimation,
+                    ]}/>
+            </View>
+            <Animated.View style={lottieAnimationStyle}>
                 <LottieView
                     source={item.animation} 
                     style={{width: SCREEN_WIDTH * 0.9, height: SCREEN_WIDTH * 0.9}}
                     autoPlay
                     loop
                 />
-            </View>  
+            </Animated.View>  
             <Text style={[styles.itemText, {color: item.textColor}]}>{item.text}</Text>
         </View>
     )
@@ -36,10 +82,14 @@ const styles = StyleSheet.create({
     },
     itemText: {
         textAlign: 'center',
-        fontSize: 44,
+        fontSize: 28,
         fontWeight: 'bold',
         marginBottom: 10,
         marginHorizontal: 20,
-
+    },
+    circleContainer: {
+        ...StyleSheet.absoluteFillObject,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     }
 })
